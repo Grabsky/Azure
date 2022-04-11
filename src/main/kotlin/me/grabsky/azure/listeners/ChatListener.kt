e4ba40c9a41ebdbf1082231f7be8e4ea1db4f325
Grabsky <44530932+Grabsky@users.noreply.github.com>
2022-04-11 22:36:59 +0200

    minor progress on chat "module"

--- src/main/kotlin/me/grabsky/azure/listeners/ChatListener.kt
+++ src/main/kotlin/me/grabsky/azure/listeners/ChatListener.kt
@@ -1,11 +1,35 @@
 package me.grabsky.azure.listeners
 
+import io.papermc.paper.chat.ChatRenderer
 import io.papermc.paper.event.player.AsyncChatEvent
+import me.grabsky.azure.configuration.AzureConfig
 import me.grabsky.azure.configuration.AzureLocale
 import me.grabsky.indigo.extensions.sendMessageOrIgnore
+import me.grabsky.indigo.utils.parseComponent
+import net.kyori.adventure.audience.Audience
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder
+import net.luckperms.api.LuckPermsProvider
+import org.bukkit.command.ConsoleCommandSender
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import java.util.*
+
+val luckPerms = LuckPermsProvider.get()
+
+// TO-DO: Generate LP group resolvers dynamically
+class GameChatRenderer : ChatRenderer {
+    override fun render(source: Player, sourceDisplayName: Component, message: Component, viewer: Audience): Component {
+        if (viewer !is ConsoleCommandSender) {
+            return parseComponent(text = AzureConfig.CHAT_FORMAT_DEFAULT,
+                Placeholder.unparsed("playername", source.name),
+                // Placeholder.unparsed("group", luckPerms)
+            ).append(message)
+        }
+        return Component.text("//")
+    }
+}
 
 class ChatListener : Listener {
     val cooldowns: MutableMap<UUID, Long> = mutableMapOf()
@@ -19,6 +43,7 @@ class ChatListener : Listener {
             return
         }
         cooldowns[event.player.uniqueId] = System.currentTimeMillis()
-        // TO-DO: Chat format logic
+        // Setting the renderer
+        // event.renderer(GameChatRenderer)
     }
 }
