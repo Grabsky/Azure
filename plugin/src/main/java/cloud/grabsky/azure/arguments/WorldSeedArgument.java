4fa43094f46ca1900392e1a9ef791194b4f31147
Michał Czopek <44530932+Grabsky@users.noreply.github.com>
2023-04-02 19:09:06 +0200

    remove @r from /world create command

--- plugin/src/main/java/cloud/grabsky/azure/arguments/WorldSeedArgument.java
+++ plugin/src/main/java/cloud/grabsky/azure/arguments/WorldSeedArgument.java
@@ -20,18 +20,16 @@ public enum WorldSeedArgument implements ArgumentParser<Long>, CompletionsProvid
     @Override
     public @NotNull List<String> provide(final @NotNull RootCommandContext context) {
         return (context.getExecutor().isPlayer() == true)
-                ? List.of("@r", "@random", String.valueOf(context.getExecutor().asPlayer().getWorld().getSeed()))
-                : List.of("@r", "@random");
+                ? List.of("@random", String.valueOf(context.getExecutor().asPlayer().getWorld().getSeed()))
+                : List.of("@random");
     }
 
     @Override
     public Long parse(final @NotNull RootCommandContext context, final @NotNull ArgumentQueue arguments) throws ArgumentParseException, MissingInputException {
         final String value = arguments.nextString();
         try {
-            return switch (value) {
-                case "@r", "@random" -> new Random().nextLong();
-                default -> Long.parseLong(value);
-            };
+            return (value.equalsIgnoreCase("@random") == true)
+                ? new Random().nextLong() : Long.parseLong(value);
         } catch (final NumberFormatException exc) {
             throw new WorldSeedArgument.Exception(value, exc);
         }
