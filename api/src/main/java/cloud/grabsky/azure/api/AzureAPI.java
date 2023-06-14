dca57d08bdcf79d91ae0832de605a38350d0f82e
Michał Czopek <44530932+Grabsky@users.noreply.github.com>
2023-06-14 11:21:27 +0200

    expose WorldManager to AzureAPI

--- api/src/main/java/cloud/grabsky/azure/api/AzureAPI.java
+++ api/src/main/java/cloud/grabsky/azure/api/AzureAPI.java
@@ -1,9 +1,12 @@
 package cloud.grabsky.azure.api;
 
 import cloud.grabsky.azure.api.user.UserCache;
+import cloud.grabsky.azure.api.world.WorldManager;
 
 public interface AzureAPI  {
 
     UserCache getUserCache();
+
+    WorldManager getWorldManager();
 
 }
