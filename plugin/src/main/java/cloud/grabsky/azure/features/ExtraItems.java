ff76da1bc60cb686deab59689edc7dd424e49e5c
Michał Czopek <44530932+Grabsky@users.noreply.github.com>
2023-08-03 11:42:44 +0200

    preliminary impl of armor effects

---
+++ plugin/src/main/java/cloud/grabsky/azure/features/ExtraItems.java
@@ -0,0 +1,50 @@
+package cloud.grabsky.azure.features;
+
+import cloud.grabsky.azure.Azure;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.event.Listener;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitTask;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+// NOTE: The reason why this is hard-coded is because there is pretty much nothing to change.
+// NOTE: Implementation does not use PlayerArmorChangeEvent because of following reasons:
+//         - Current implementation shouldn't have much impact on performance.
+//         - Current implementation is simple and cannot be exploited.
+//         - Mentioned event does not cover all possible cases of the armor change. (though it may in the future)
+@RequiredArgsConstructor(access = AccessLevel.PUBLIC)
+public final class ExtraItems implements Listener {
+
+    private final @NotNull Azure plugin;
+
+    private static @Nullable BukkitTask TASK = null;
+
+    private static final NamespacedKey KEY_HAS_JUMP_BOOST = new NamespacedKey("azure", "item/has_jump_boost");
+    private static final NamespacedKey KEY_HAS_SPEED_BOOST = new NamespacedKey("azure", "item/has_speed_boost");
+
+    public void initialize() {
+        // Cancelling existing task to make sure logic is not duplicated.
+        if (TASK != null)
+            TASK.cancel();
+        // Scheduling a new repeating task.
+        TASK = plugin.getBedrockScheduler().repeat(20L, 20L, Long.MAX_VALUE, (___) -> {
+            Bukkit.getOnlinePlayers().forEach(player -> {
+                if (player.getInventory().getBoots() != null && player.getInventory().getBoots().hasItemMeta() == true) {
+                    final PersistentDataContainer container = player.getInventory().getBoots().getItemMeta().getPersistentDataContainer();
+                    if (container.has(KEY_HAS_SPEED_BOOST) == true)
+                        player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 21, 0, false, false, false));
+                    if (container.has(KEY_HAS_JUMP_BOOST) == true)
+                        player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 21, 2, false, false, false));
+                }
+            });
+            return true;
+        });
+    }
+
+}
