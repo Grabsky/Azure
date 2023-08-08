5bb6ae772dcaa294f64d6248f030db95931c2ecd
Michał Czopek <44530932+Grabsky@users.noreply.github.com>
2023-08-08 10:44:59 +0200

    add /debug command

--- plugin/src/main/java/cloud/grabsky/azure/commands/DebugCommand.java
+++ plugin/src/main/java/cloud/grabsky/azure/commands/DebugCommand.java
@@ -1,22 +1,24 @@
 package cloud.grabsky.azure.commands;
 
 import cloud.grabsky.azure.Azure;
+import cloud.grabsky.azure.features.DragonEvent;
 import cloud.grabsky.bedrock.components.Message;
 import cloud.grabsky.commands.ArgumentQueue;
 import cloud.grabsky.commands.RootCommand;
 import cloud.grabsky.commands.RootCommandContext;
 import cloud.grabsky.commands.component.CompletionsProvider;
 import cloud.grabsky.commands.exception.CommandLogicException;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class DebugCommand extends RootCommand implements Listener {
 
@@ -28,9 +30,19 @@ public class DebugCommand extends RootCommand implements Listener {
         this.plugin = plugin;
     }
 
+    private static @Nullable DragonEvent DRAGON_EVENT;
+
     @Override
     public @NotNull CompletionsProvider onTabComplete(final @NotNull RootCommandContext context, final int index) throws CommandLogicException {
-        return (index == 0) ? CompletionsProvider.of("refresh_listeners", "refresh_recipes", "delete_entity") : CompletionsProvider.EMPTY;
+        if (index == 0)
+            return CompletionsProvider.of("refresh_listeners", "refresh_recipes", "delete_entity", "dragon", "modify");
+        // ...
+        final String literal = context.getInput().at(1).toLowerCase();
+        // ...
+        return switch (literal) {
+            case "dragon" -> (index == 1) ? CompletionsProvider.of("start", "cancel", "refresh") : CompletionsProvider.EMPTY;
+            default -> CompletionsProvider.EMPTY;
+        };
     }
 
     @Override
@@ -52,7 +64,7 @@ public class DebugCommand extends RootCommand implements Listener {
                 case "delete_entity" -> {
                     if (sender instanceof Player senderPlayer) {
                         final Entity entity = senderPlayer.getTargetEntity(20);
-                        if (entity instanceof LivingEntity == true)
+                        if (entity != null)
                             entity.remove();
                     }
                 }
@@ -69,6 +81,24 @@ public class DebugCommand extends RootCommand implements Listener {
                         item.editMeta(meta -> {
                             final PersistentDataContainer container = meta.getPersistentDataContainer();
                         });
+                    }
+                }
+                case "dragon" -> {
+                    switch (arguments.next(String.class).asRequired().toLowerCase()) {
+                        case "start" -> {
+                            if (DRAGON_EVENT == null)
+                                DRAGON_EVENT = new DragonEvent(plugin, Bukkit.getWorld("world_the_end"));
+                        }
+                        case "cancel" -> {
+                            if (DRAGON_EVENT != null)
+                                DRAGON_EVENT.cancel();
+                            // ...
+                            DRAGON_EVENT = null;
+                        }
+                        case "refresh" -> {
+                            if (DRAGON_EVENT != null)
+                                DRAGON_EVENT.refresh();
+                        }
                     }
                 }
             }
